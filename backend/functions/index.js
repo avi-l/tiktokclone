3003121904c6855cefc09607de57f7d70a151029
Avi Leeker <avi.leeker@gmail.com>
2022-05-24 08:53:19 +0300

    initial commit

---
+++ backend/functions/index.js
@@ -0,0 +1,12 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const db = admin.firestore();
+
+exports.newUser = functions.auth.user().onCreate((user) => {
+  return db
+      .collection("user")
+      .doc(user.uid)
+      .create(JSON.parse(JSON.stringify(user)));
+});
